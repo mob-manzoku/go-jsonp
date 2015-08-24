5aa6324ef4a121935ba33375c4972516a8f2e52d
Ryo MANZOKU <manzoku@mobcast.jp>
2015-08-24 11:52:17 +0000

    1st commit

---
+++ go-jsonp.go
@@ -0,0 +1,35 @@
+package gojsonp
+
+import (
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+var exceptCallback = regexp.MustCompile(`callback\((.*)\)`)
+var jsonize = regexp.MustCompile("([0-9a-zA-Z]*):([0-9a-zA-Z]*)")
+
+func GetStringFromJsonpString(str string) string {
+	excepted := exceptCallback.FindAllStringSubmatch(str, -1)[0][1]
+
+	return jsonize.ReplaceAllString(excepted, "\"${1}\":${2}")
+
+}
+
+func GetStringFromURL(url string) string {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	str := string(body)
+
+	return GetStringFromJsonpString(str)
+}
